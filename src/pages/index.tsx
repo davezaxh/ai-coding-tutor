10e7278dc9ed7f2acb558bc7d76eea86fce0b6b1
Prajwal <prajwify@gmail.com>
2024-04-04 17:54:52 +0530

    progress 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 export default function index() {
   const [code, setCode] = useState("");
   const [problemStatement, setProblemStatement] = useState("");
+  const [language, setLanguage] = useState<string | null>(null);
 
   //Temporarily set a random problem statement
   useEffect(() => {
@@ -11,7 +12,7 @@ export default function index() {
   }, [])
 
   return (
-    <div className='p-4'>
+    <div className='p-3'>
       <div>
         <h1 className='text-4xl font-bold'>AI Coding Tutor</h1>
       </div>
@@ -20,15 +21,26 @@ export default function index() {
           <h1 className='text-xl font-semibold'>Problem Statement : </h1>
           <p>{problemStatement}</p>
         </div>
-        <div className='grid grid-cols-12'>
-          <div className='col-span-9 bg-gray-200 p-4 rounded-xl'>
+        <div className='h-[60vh] grid grid-cols-12 gap-2'>
+          <div className='col-span-8 bg-gray-200 p-4 rounded-xl'>
+            <div className='pb-3 flex flex-row gap-2'>
+              <h1 className='p-2'>Select Language : </h1>
+              <select className='p-2 bg-gray-100 rounded-lg' onChange={(e) => setLanguage(e.target.value)}>
+                <option value="js">Javascript</option>
+                <option value="py">Python</option>
+                <option value="cpp">C++</option>
+              </select>
+            </div>
             <ReactCodeMirror
+              theme="dark"
               value={code}
             />
           </div>
-          <div className='col-span-3'>
-
+          <div className='col-span-4 bg-gray-200 p-4 rounded-xl'>
+            <h1>Hints & Solutions</h1>
           </div>
+        </div>
+        <div className='p-2 bg-gray-700 h-[20vh]'>
         </div>
       </div>
     </div>
