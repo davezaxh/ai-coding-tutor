fa63227dbe53429465a4299c28a607030aed07c8
davezaxh <zachariahdave@gmail.com>
2024-04-05 18:37:38 +0530

    fix(compile endpoint): returns the correct output

--- src/pages/api/compile.ts
+++ src/pages/api/compile.ts
@@ -16,19 +16,17 @@ export default async function compileCode(req: NextApiRequest, res: NextApiRespo
     console.log(data);
     
     let output;
-    do {
-        const request = await fetch(`${baseurl}/submissions/batch?tokens=${data.token}`);
-        output = await request.json();
-    } while (output.submissions[0].status.id !== 3); 
+    await new Promise(resolve => setTimeout(resolve, 2000)); // Wait for 1 second
+    const request = await fetch(`${baseurl}/submissions/batch?tokens=${data.token}`);
+    output = await request.json();
 
     if(output.submissions[0].stdout === null){
-        res.status(200).json({stdout: output.submissions[0].stderr});
+        res.status(200).json({stderr: output.submissions[0].stderr});
     }
     else if(output.submissions[0].stderr === null){
-        res.status(200).json({stderr: output.submissions[0].stdout});   
+        res.status(200).json({stdout: output.submissions[0].stdout});   
     }
 
-    
     console.log(output);
 
     }
