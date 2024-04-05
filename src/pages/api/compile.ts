0ff225a43d4342fb5dce2ded148c8e59d8d70aad
davezaxh <zachariahdave@gmail.com>
2024-04-05 14:47:22 +0530

    added commit endpoint

---
+++ src/pages/api/compile.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function compileCode(req: NextApiRequest, res: NextApiResponse) {
+    const { code, lid } = req.body;
+    const baseurl = process.env.JUDGE0_URL;
+
+    const response = await fetch(`${baseurl}/submissions`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({source_code : code, language_id : lid}),
+    });
+    
+    const data = await response.json();
+    console.log(data);
+    
+    let output;
+    do {
+        const request = await fetch(`${baseurl}/submissions/batch?tokens=${data.token}`);
+        output = await request.json();
+    } while (output.submissions[0].status.id !== 3); 
+
+    if(output.submissions[0].stdout === null){
+        res.status(200).json({stdout: output.submissions[0].stderr});
+    }
+    else if(output.submissions[0].stderr === null){
+        res.status(200).json({stderr: output.submissions[0].stdout});   
+    }
+
+    
+    console.log(output);
+
+    }
