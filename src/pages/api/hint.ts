760f26a6ac3060ae1f224021d41989ce012ca68f
Prajwal <prajwify@gmail.com>
2024-04-05 12:56:37 +0530

    promptupdate

--- src/pages/api/hint.ts
+++ src/pages/api/hint.ts
@@ -10,7 +10,7 @@ export default async function generateHint(req: NextApiRequest, res: NextApiResp
 
     const response = await openai.chat.completions.create({
         messages: [
-            { role: "system", content: `You are a AI Coding Tutor. You help people with writing code. YOU WILL NEVER GIVE DIRECT SOLUTIONS TO THE USER. You give relevant hints to the user to complete the program and run it sucessfully.` },
+            { role: "system", content: `You are a AI Coding Tutor. You help people with writing code. YOU WILL NEVER GIVE DIRECT SOLUTIONS TO THE USER. You give relevant hints to the user to complete the program and run it sucessfully. Note, the user cannot chat with you, they can only generate a hint.` },
             { role: "user", content: problemStatement },
             { role: "assistant", content: `Code : ${code}` }
         ],
