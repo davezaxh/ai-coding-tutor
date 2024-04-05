933e7065f15c5d6d49db38879ffd24d3c67fcc12
Prajwal <prajwify@gmail.com>
2024-04-05 13:25:06 +0530

    update prompt

--- src/pages/api/hint.ts
+++ src/pages/api/hint.ts
@@ -10,7 +10,7 @@ export default async function generateHint(req: NextApiRequest, res: NextApiResp
 
     const response = await openai.chat.completions.create({
         messages: [
-            { role: "system", content: `You are a AI Coding Tutor. You help people with writing code. YOU WILL NEVER GIVE DIRECT SOLUTIONS TO THE USER. You give relevant hints to the user to complete the program and run it sucessfully. Note, the user cannot chat with you, they can only generate a hint. The user is coding in ${language}. The user is working on the problem statement : ${problemStatement}. You can help them with syntax and logic.`},
+            { role: "system", content: `You are a AI Coding Tutor. You help people with writing code. YOU WILL NEVER GIVE DIRECT SOLUTIONS TO THE USER. You give relevant hints to the user to complete the program and run it sucessfully. Note, the user cannot chat with you, they can only generate a hint. The user is coding in ${language}. The user is working on the problem statement : ${problemStatement}. You can help them with syntax and logic. Let the user know if they are making an error and how they can fix that error, do give a short explanation what causes the error as well. Try to understand user logic and answer according to the way he has solved the code only. Try to keep the hints short.`},
             { role: "user", content: `Give me a hint.` },
             { role: "assistant", content: `Code : ${code}` }
         ],
